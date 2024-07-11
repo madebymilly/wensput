28cdfaf1090e39f3c5093651d9a55f2be0409715
Milly Held <hallo@millyheld.nl>
2024-07-11 13:47:43 +0200

    Add helper functions for shuffle and getRandomItems

---
+++ de-wensput/src/js/helpers.js
@@ -0,0 +1,23 @@
+export function getRandomItems(arr, numItems) {
+  // Maak een kopie van de array om mutaties te voorkomen
+  let arrCopy = [...arr];
+
+  // Controleer of het aantal gevraagde items niet groter is dan de array-lengte
+  if (numItems > arr.length) {
+    throw new Error("Het aantal gevraagde items is groter dan de array-lengte.");
+  }
+
+  // Schud de array (Fisher-Yates shuffle algoritme)
+  shuffle(arrCopy)
+
+  // Retourneer de eerste numItems elementen van de geschudde array
+  return arrCopy.slice(0, numItems);
+}
+
+export function shuffle(arr) {
+  // Fisher-Yates shuffle algoritme
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
