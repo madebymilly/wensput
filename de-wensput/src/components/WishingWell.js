1a56a7e11875dc22e5a24d36912f1ffe9a406bc3
Milly Held <contact@buroaandelaan.nl>
2025-08-20 13:49:40 +0200

    Move LED to useEffect, add transition fadeIn/out

--- de-wensput/src/components/WishingWell.js
+++ de-wensput/src/components/WishingWell.js
@@ -1,37 +1,46 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef, createRef } from 'react';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
-import Header from './Header'
-import Question from './Question'
-import Start from './Start'
-import End from './End'
-import AudioPlayer from './AudioPlayer'
+import Header from './Header';
+import Question from './Question';
+import Start from './Start';
+import End from './End';
+import AudioPlayer from './AudioPlayer';
 
-// import Test from './test-components/Test'
-
-import { shuffle } from '../js/helpers'
-import { addMediumTwinkle, addFastTwinkle, addMultiColor, turnLedOff } from '../js/led'
-import { filterProducts } from '../js/filterProducts'
-import { checkAudioFile } from '../js/checkAudioFile'
-import { getQuestions } from '../js/getQuestions'
+import { shuffle } from '../js/helpers';
+import { addMediumTwinkle, addFastTwinkle, addMultiColor, turnLedOff } from '../js/led';
+import { filterProducts } from '../js/filterProducts';
+import { checkAudioFile } from '../js/checkAudioFile';
+import { getQuestions } from '../js/getQuestions';
 
 import { NUM_OF_Q } from '../config/settings';
 
 function WishingWell({ allProducts }) {
-
-  const [started, setStarted] = useState(false)
-  const [ended, setEnded] = useState(false)
-  const [products, setProducts] = useState([])
-  const [currentQuestionId, setCurrentQuestionId] = useState(1)
-  const [wish, setWish] = useState({})
+  const [started, setStarted] = useState(false);
+  const [ended, setEnded] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [currentQuestionId, setCurrentQuestionId] = useState(1);
+  const [wish, setWish] = useState({});
   const [questions, setQuestions] = useState([]);
 
+  // ---- Transition refs per "scherm" (start/end/question)
+  const nodeRefs = useRef(new Map());
+  const getNodeRef = (key) => {
+    if (!nodeRefs.current.has(key)) {
+      nodeRefs.current.set(key, createRef());
+    }
+    return nodeRefs.current.get(key);
+  };
+
+  // ---- Einde bepaald door aantal vragen
   useEffect(() => {
     if (currentQuestionId > NUM_OF_Q) {
-      setEnded(true)
-      getWish()
+      setEnded(true);
+      getWish();
     }
   }, [currentQuestionId]);
 
+  // ---- Products met audio filteren + shufflen
   useEffect(() => {
     const filterProductsWithAudio = async () => {
       const newProducts = await Promise.all(
@@ -40,77 +49,101 @@ function WishingWell({ allProducts }) {
           return audioFileExists ? product : null;
         })
       );
-
       const shuffledProducts = shuffle(newProducts.filter(Boolean));
-
       setProducts(shuffledProducts);
     };
     filterProductsWithAudio();
   }, [allProducts]);
 
+  // ---- LED side effects netjes via effects i.p.v. in render
+  useEffect(() => {
+    if (!started) {
+      turnLedOff();
+    }
+  }, [started]);
+
+  useEffect(() => {
+    if (ended) {
+      // addFastTwinkle();
+      addMultiColor(); // teststand
+    }
+  }, [ended]);
+
+  // ---- Transition key verandert automatisch bij state-wissel
+  const transitionKey = !started ? 'start' : ended ? 'end' : `q-${currentQuestionId}`;
+  const nodeRef = getNodeRef(transitionKey);
+
   return (
     <>
       <Header current={currentQuestionId} total={NUM_OF_Q} showProgress={started && !ended} />
+
       <div className="content">
-        {renderContent()}
+        <SwitchTransition mode="out-in">
+          <CSSTransition
+            key={transitionKey}
+            nodeRef={nodeRef}
+            classNames="fade"
+            timeout={300}
+            appear
+            unmountOnExit
+          >
+            <div ref={nodeRef}>
+              {renderContent()}
+            </div>
+          </CSSTransition>
+        </SwitchTransition>
       </div>
-      <AudioPlayer audioSrc="/audio/music.mp3" play={started} volume={0.4} loop={true} startFrom={60.7} lowerVolume={ended} />
+
+      <AudioPlayer
+        audioSrc="/audio/music.mp3"
+        play={started}
+        volume={0.4}
+        loop={true}
+        startFrom={60.7}
+        lowerVolume={ended}
+      />
       {/* <Test products={products} /> */}
     </>
-  )
+  );
 
   function renderContent() {
     if (!started) {
-      // turn off leds:
-      turnLedOff();
       return <Start handleClick={handleStartClick} />;
     }
     if (ended) {
-      //addFastTwinkle()
-      addMultiColor() // this is just for testing to see a bigger difference in leds
       return <End wish={wish} handleClick={handleEndClick} />;
     }
     return questions
-      .filter(question => question.id === currentQuestionId)
-      .map((question, i) => (
+      .filter((question) => question.id === currentQuestionId)
+      .map((question) => (
         <Question
-          key={i}
+          key={question.id}
           {...question}
           handleClick={handleAnswerClick}
         />
       ));
-  };
+  }
 
   function getWish() {
     if (products.length > 0) {
-      setWish(products[0]); // Get the first item of products, because is sorted and shuffled
+      setWish(products[0]); // eerst item is al gesorteerd/geschud
     } else {
       setWish(false);
     }
   }
 
   function handleAnswerClick(answer) {
-
-    // Filter products based on answer:
     const filteredProducts = filterProducts(products, answer);
-
-    // Set state of products:
     setProducts(filteredProducts);
-
-    // Go to next question:
-    setCurrentQuestionId(currentQuestionId + 1);
-
-    console.log(answer)
-
+    setCurrentQuestionId((id) => id + 1);
+    console.log(answer);
   }
 
   function handleStartClick() {
-    setStarted(true)
-    setQuestions(getQuestions())
-
-    addMediumTwinkle()
-
-    console.log(products)
+    setStarted(true);
+    setQuestions(getQuestions());
+    addMediumTwinkle();
+    console.log(products);
   }
 
   function handleEndClick() {
