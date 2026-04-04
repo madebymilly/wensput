b13d104278607a75418d02b45947507ef817ebdc
Milly Held <contact@buroaandelaan.nl>
2026-04-04 12:20:14 +0200

    Change check for Audiofile

--- de-wensput/src/js/checkAudioFile.js
+++ de-wensput/src/js/checkAudioFile.js
@@ -1,8 +1,20 @@
+// export const checkAudioFile = async (slug) => {
+//   const response = await fetch(`/audio/games/${slug}.mp3`);
+//   if (response.ok && response.headers.get('Content-Type') === 'audio/mpeg') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
 export const checkAudioFile = async (slug) => {
-  const response = await fetch(`/audio/games/${slug}.mp3`);
-  if (response.ok && response.headers.get('Content-Type') === 'audio/mpeg') {
-    return true;
-  } else {
+  try {
+    const response = await fetch(`/audio/games/${slug}.mp3`, {
+      method: 'HEAD',
+    });
+    //console.log(`Checked audio for ${slug}:`, response.ok);
+    return response.ok;
+  } catch (error) {
     return false;
   }
 };
